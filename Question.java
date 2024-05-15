4a190e63755bd8974152cfa33ce56e0d347207ca
tsaranto <119889560+tsaranto@users.noreply.github.com>
2024-05-15 14:07:49 +0300

    Add files via upload

--- Question.java
+++ Question.java
@@ -1,9 +1,35 @@
-public class Question {
+public abstract class Question {
     int code;
-    String cue;
+    String description;
 
-    public Question(int code, String cue) {
+    public Question(int code, String description) {
         this.code = code;
-        this.cue = cue;
+        this.description = description;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public abstract boolean checkAnswer(String answer);
+
+    @Override
+    public String toString() {
+        return "Question{" +
+               "code=" + code +
+               ", description='" + description + '\'' +
+               '}';
     }
 }
