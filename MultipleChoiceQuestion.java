aa97a41717fb6a7022a59fa8ca93e884ba0f5242
elmosss <elmaroussg@gmail.com>
2024-05-18 19:13:37 +0300

    Add files via upload

--- MultipleChoiceQuestion.java
+++ MultipleChoiceQuestion.java
@@ -1,4 +1,17 @@
-public class MultipleChoiceQuestion {
+public class MultipleChoiceQuestion extends Question{
     String answerlist;
     String correctanswer;
 }
+public MultipleChoiceQuestion(String answerlist,String correctanswer , int code, String description) {
+
+    super(code , description);
+    this.answerlist = answerlist;
+    this.correctanswer = correctanswer;
+
+}
+
+public int checkAnswer(){
+
+
+}
+
