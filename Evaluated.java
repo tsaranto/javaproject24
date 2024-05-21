ac55ff8681c4af42b7010eb5a40392107a77495b
tsaranto <119889560+tsaranto@users.noreply.github.com>
2024-05-21 21:36:53 +0300

    douleia mexri twra/oxi teleiwmenes ejiswseis

--- Evaluated.java
+++ Evaluated.java
@@ -9,5 +9,36 @@ public class Evaluated {
         this.surname = surname;
     }
 
-    
+    public int getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    @Override
+    public String toString() {
+        return "Evaluated{" +
+                "code=" + code +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                '}';
+    }
 }
